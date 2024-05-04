1cad69b57b848b5d2922bc87f9c9f4c044aea44a
André Polomat <andre.polomat@gearboxsoftware.com>
2024-05-03 21:55:00 -0400

    Removed broken song and added delayed update system to the sound controller

--- src/Sounds.hpp
+++ src/Sounds.hpp
@@ -4,6 +4,8 @@
 
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
+#include "debug.hpp"
+#include "settings.hpp"
 
 enum class Song : uint8_t
 {
@@ -16,8 +18,9 @@ enum class Song : uint8_t
     Reaction_01 = Reaction_Start,
     Reaction_02,
     Reaction_03,
-    Reaction_04,
-    Reaction_End = Reaction_04,
+    Reaction_End = Reaction_03,
+
+    None
 };
 
 class SoundController
@@ -25,37 +28,133 @@ class SoundController
     SoftwareSerial serial = {3, 4}; // RX, TX
     DFRobotDFPlayerMini player;
 
+    bool isInit = false;
+
+    uint8_t volume = -1;
+    uint8_t wantVolume;
+
+    bool isLoop = false;
+    bool wantLoop = true;
+
+    Song song = Song::None;
+    Song wantSong = Song::None;
+
+    enum class State
+    {
+        Playing,
+        Paused,
+        Stopped
+    };
+
+    State state = State::Stopped;
+    State wantState = State::Stopped;
+
+    uint32_t lastUpdate = 0;
+    uint32_t updateDelay = 300;
+
 public:
     void init()
     {
         serial.begin(9600);
+    }
 
-        while (!player.begin(serial, false))
+    void update()
+    {
+        const auto now = millis();
+        if(now - lastUpdate < updateDelay)
         {
-            delay(500);
+            return;
         }
 
-        player.volume(20); //Set volume value. From 0 to 30
-        //player.outputSetting(false, 31);  //Set volume value. From 0 to 30
+        lastUpdate = now;
+
+        if(debug::fastPath)
+        {
+            wantVolume = 5;
+        }
+        else
+        {
+            wantVolume = settings.volume; 
+        }
+
+        if(!isInit)
+        {
+            isInit = player.begin(serial, false);
+            return;
+        }
+
+        if(isLoop != wantLoop)
+        {
+            isLoop = wantLoop;
+            if(isLoop)
+            {
+                player.enableLoop();
+            }
+            else
+            {
+                player.disableLoop();
+            }
+
+            return;
+        }
+
+        if(volume != wantVolume)
+        {
+            volume = wantVolume;
+            player.volume(volume); //Set volume value. From 0 to 30
+
+            return;
+        }
+
+        if(wantSong != song)
+        {
+            song = wantSong;
+            state = State::Playing;
+
+            player.play(static_cast<uint8_t>(song));
+
+            return;
+        }
+
+        if(wantState != state)
+        {
+            state = wantState;
+            if(state == State::Paused)
+            {
+                player.pause();
+            }
+            else if(state == State::Playing)
+            {
+                player.start();
+            }
+            else if(state == State::Stopped)
+            {
+                song = Song::None;
+                player.stop();
+            }
+
+            return;
+        }
     }
 
     void play(Song song)
     {
-        player.play(static_cast<uint8_t>(song));
+        wantSong = song;
+        wantState = State::Playing;
     }
 
     void pause()
     {
-        player.pause();
+        wantState = State::Paused;
     }
 
     void resume()
     {
-        player.start();
+        wantState = State::Playing;
     }
 
     void stop()
     {
-        player.stop();
+        wantState = State::Stopped;
     }
 };
